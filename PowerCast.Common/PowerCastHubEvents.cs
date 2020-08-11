e30e1e68a67840cffc50a7f6ceba0c19f0e202c7
Kustas, George <gkustas@gmail.com>
2020-08-11 16:48:40 -0400

    updated event list. Added gitignore

--- PowerCast.Common/PowerCastHubEvents.cs
+++ PowerCast.Common/PowerCastHubEvents.cs
@@ -5,10 +5,7 @@
 		public const string DiagnosticReportOpen = "DiagnosticReport-open";
 		public const string DiagnosticReportClose = "DiagnosticReport-close";
 		public const string DiagnosticReportUpdate = "DiagnosticReport-update";
-		public const string DiagnosticReportGet = "DiagnosticReport-get";
-		public const string DiagnosticReportSet = "DiagnosticReport-set";
 		public const string UserLogout = "userlogout";
-		public const string SyncError = "syncerror";
 		public const string DmvaMessage = "DMVAMessage";
 	}
 }
