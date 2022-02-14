ad7a5a86fb6b9ddb14756429bb531a033585cc71
Jon Ladd <Jon.Ladd@nuance.com>
2022-02-14 14:16:57 -0500

    Updates to use context version id/previous version id. Updated hl7 and newtonsoft package references.

--- PowerCast.Common/PowerCastHubEvents.cs
+++ PowerCast.Common/PowerCastHubEvents.cs
@@ -5,7 +5,18 @@
 		public const string DiagnosticReportOpen = "DiagnosticReport-open";
 		public const string DiagnosticReportClose = "DiagnosticReport-close";
 		public const string DiagnosticReportUpdate = "DiagnosticReport-update";
+		public const string DiagnosticReportSelect = "DiagnosticReport-select";
+
+		public const string ImagingStudyOpen = "ImagingStudy-open";
+		public const string ImagingStudyClose = "ImagingStudy-close";
+
+		public const string PatientOpen = "Patient-open";
+		public const string PatientClose = "Patient-close";
+
 		public const string UserLogout = "userlogout";
+		public const string UserHibernate = "userhibernate";
+
+		public const string SyncError = "syncerror";
 		public const string DmvaMessage = "DMVAMessage";
 	}
 }
